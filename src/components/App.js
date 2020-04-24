7b4cfe480634448d6a7a0d0106bbf0977c1b0038
kims3939 <kims3939@gmail.com>
2020-04-24 09:26:01 +0900

    merge finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CaseList from './CaseListPage/CaseList';
-import { Grid } from '@material-ui/core';
+import { Grid, Hidden } from '@material-ui/core';
+
+import CaseDesktopAppBar from './CaseAppBar/CaseDesktopAppBar';
+import CaseMobileAppBar from './CaseAppBar/CaseMobileAppBar';
 
 function App() {
   return (
         <>
+            <Hidden only={['xs']}><CaseDesktopAppBar /></Hidden>
+            <Hidden only={['xl','lg','md','sm']}><CaseMobileAppBar /></Hidden>
             <Grid container direction='row'>
                 <Grid item xl={4} lg={3} md={3}></Grid>
                 <Grid item xl={4} lg={6} md={6} sm={12} xs={12}><CaseList /></Grid>
